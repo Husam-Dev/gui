6f1f9c89566db794dfd671cafeb4923162e410e1
Husam_Dev <72893731+Husam-Dev@users.noreply.github.com>
2022-09-22 14:18:05 +0200

    Update vault.lua

--- vault.lua
+++ vault.lua
@@ -1,3 +1,3 @@
-getgenv().CASHTPCustomLocationX = -503.6231689453125
-getgenv().CASHTPCustomLocationY = 23.155654907226562
-getgenv().CASHTPCustomLocationZ = -285.03924560546875
+getgenv().thex = -503.6231689453125
+getgenv().they = 23.155654907226562
+getgenv().thez = -285.03924560546875
